f91b97d128b44c645e92de68802fb8514d7c41fd
KovGr <KovGrDm@outlook.com>
2024-03-18 15:25:03 +0200

    Botexample1

--- src/main/java/projekt/telegram/BotConstans.java
+++ src/main/java/projekt/telegram/BotConstans.java
@@ -4,6 +4,6 @@ public class BotConstans {
     public static final String BOT_NAME = "ExampleCurrencybot";
 
     
-    public static final String BOT_TOKEN = "6731462935:AAGGeAfvJynh0QfJBLEGCToOztBoe1ylQHA";
+    public static final String BOT_TOKEN = "";
 
 }
